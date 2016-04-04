697e6c49b500e3f0755d00d34424d1d7c48a97c1
Alessio Fanelli <fana@alessiofanelli.com>
2016-04-04 15:04:29 +0200

    feat(design): card design
    Card design completed
    Missing:
    - Actions
    - Price showing

--- app/components/route-display.js
+++ app/components/route-display.js
@@ -1,9 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  className: 'route-display',
+
   didReceiveAttrs() {
     var _this = this;
     var classes_json = this.get('classes');
+    this.set('logo', 'http://alessiofanelli.com/wd/' + this.get('agency') + '.jpg');
+    var minimum = null;
+
+    let duration = this.get('duration'),
+        hours = Math.floor(duration/ 60),
+        minutes = duration % 60;
+    this.set('formattedDuration', hours + 'h ' + minutes + 'm');
 
     var seconda = classes_json[0]['fares'],
         prima = classes_json[1]['fares'],
@@ -18,10 +27,13 @@ export default Ember.Component.extend({
       let fare = 'seconda' + entry['name'];
       if (entry['price']){
           _this.set(fare, entry['price']['amount']);
+          if (minimum == null) { minimum = entry['price']['amount']}
       } else {
           _this.set(fare, '-');
       }
     });
+
+    this.set('wholePrice', minimum);
 
     prima.forEach(function(entry) {
       if (entry['name'] == 'Low Cost') {
@@ -53,7 +65,7 @@ export default Ember.Component.extend({
       if (entry['name'] == 'Low Cost') {
         entry['name'] = 'SuperEconomy'
       }
-      
+
       let fare = 'club' + entry['name'];
       if (entry['price']){
           _this.set(fare, entry['price']['amount']);
