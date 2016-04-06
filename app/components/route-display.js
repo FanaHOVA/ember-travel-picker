80bc208f4793c38d71322746da6a42ef111c1fa1
Alessio Fanelli <fana@alessiofanelli.com>
2016-04-06 18:35:41 +0200

    refact(fares): more elegant fares iteration

--- app/components/route-display.js
+++ app/components/route-display.js
@@ -20,62 +20,30 @@ export default Ember.Component.extend({
     var seconda = classes_json[0]['fares'],
         prima = classes_json[1]['fares'],
         business = classes_json[2]['fares'],
-        club = classes_json[3]['fares'];
+        club = classes_json[3]['fares'],
+        minimum = null;
 
-    seconda.forEach(function(entry) {
-      if (entry['name'] == 'Low Cost') {
-        entry['name'] = 'SuperEconomy'
-      }
+    var classes = [seconda, prima, business, club],
+        fares_ids = ['seconda', 'prima', 'business', 'club']
 
-      let fare = 'seconda' + entry['name'];
-      if (entry['price']){
-          _this.set(fare, entry['price']['amount']);
-          if (minimum == null) { minimum = entry['price']['amount']}
-      } else {
-          _this.set(fare, '-');
-      }
-    });
+
+    for (let i = 0; i < 4; i++) {
+        classes[i].forEach(function(entry) {
+              if (entry['name'] == 'Low Cost') {
+                entry['name'] = 'SuperEconomy';
+              }
+
+              let fare = fares_ids[i] + entry['name'];
+              if (entry['price']){
+                  if (minimum == null) { minimum = entry['price']['amount']}
+                  _this.set(fare, entry['price']['amount']);
+              } else {
+                  _this.set(fare, '-');
+              }
+        });
+    }
 
     this.set('wholePrice', minimum);
-
-    prima.forEach(function(entry) {
-      if (entry['name'] == 'Low Cost') {
-        entry['name'] = 'SuperEconomy'
-      }
-
-      let fare = 'prima' + entry['name'];
-      if (entry['price']){
-          _this.set(fare, entry['price']['amount']);
-      } else {
-          _this.set(fare, '-');
-      }
-    });
-
-    business.forEach(function(entry) {
-      if (entry['name'] == 'Low Cost') {
-        entry['name'] = 'SuperEconomy'
-      }
-
-      let fare = 'business' + entry['name'];
-      if (entry['price']){
-          _this.set(fare, entry['price']['amount']);
-      } else {
-          _this.set(fare, '-');
-      }
-    });
-
-    club.forEach(function(entry) {
-      if (entry['name'] == 'Low Cost') {
-        entry['name'] = 'SuperEconomy'
-      }
-
-      let fare = 'club' + entry['name'];
-      if (entry['price']){
-          _this.set(fare, entry['price']['amount']);
-      } else {
-          _this.set(fare, '-');
-      }
-    });
   },
 
   actions: {
